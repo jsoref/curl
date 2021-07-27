893723d369c50f1bde4938d14ec3220e46f88f63
Josh Soref <2119212+jsoref@users.noreply.github.com>
2021-07-27 19:40:23 -0400

    Update runtests.pl

--- tests/runtests.pl
+++ tests/runtests.pl
@@ -485,7 +485,7 @@ sub startnew {
 
     logmsg "startnew: $cmd\n" if ($verbose);
 
-    exec '/bin/sh', '-c', 'date; ps -ef; lsof -iTCP -n -P';
+    system '/bin/sh', '-c', 'date; ps -ef; lsof -iTCP -n -P';
     my $child = fork();
     my $pid2 = 0;
 
